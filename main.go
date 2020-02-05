ec9a9ba8c9e9b639eff89826b884be9bf92914d7
Mr. Dung <dao.anh.dung@sun-asterisk.com>
2020-02-05 14:39:59 +0700

    refs #add Icons

--- main.go
+++ main.go
@@ -4,11 +4,37 @@ import (
     "fmt"
     "io/ioutil"
     "log"
+    "math/rand"
     "os/exec"
     "time"
 
     yaml "gopkg.in/yaml.v2"
 )
+
+var icons = []string{
+    "face-angel",
+    "face-sad",
+    "face-angry",
+    "face-sick",
+    "face-cool",
+    "face-smile-big",
+    "face-crying",
+    "face-smile",
+    "face-devilish",
+    "face-smirk",
+    "face-embarrassed",
+    "face-surprise",
+    "face-glasses",
+    "face-tired",
+    "face-kiss",
+    "face-uncertain",
+    "face-laugh",
+    "face-wink",
+    "face-monkey",
+    "face-worried",
+    "face-plain",
+    "faceaspberry",
+}
 
 type Task struct {
     Job     string `yaml:"job"`
@@ -34,7 +60,8 @@ func (y *YamlFile) readFile() {
 func checkTime(strTime string) bool {
     loc, _ := time.LoadLocation("Asia/Ho_Chi_Minh")
     t := time.Now().In(loc)
-    if strTime == t.Format("15:04") {
+    tb := t.Add(time.Duration(-15) * time.Minute)
+    if strTime == t.Format("15:04") || strTime == tb.Format("15:04") {
         return true
     }
 
@@ -42,8 +69,12 @@ func checkTime(strTime string) bool {
 }
 
 func notify(t Task) {
+    rand.Seed(time.Now().Unix())
+    n := rand.Int() % len(icons)
     cmd := exec.Command(
         "/usr/bin/notify-send",
+        "-i",
+        icons[n],
         fmt.Sprintf("%s at %s", t.Job, t.Time),
         t.Message,
     )
